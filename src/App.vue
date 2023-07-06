51b558bb950dd0d0d165541b20097a1b3606607e
DanielTwins <naseri.daniel24@gmail.com>
2023-07-06 17:32:13 +0200

    Added Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
 <!--   <EmployeeListPage_CompositionStyle /> -->
-  <EmployeeList />
+  <!-- <EmployeeList /> -->
+  <Home />
 </template>
 
 <script>
 import EmployeeListPage_CompositionStyle from "./components/EmployeeListPage_CompositionStyle.vue";
 import EmployeeList from "./components/EmployeeList.vue";
+import Home from "./views/Home.vue";
 
 export default {
   components: {
     EmployeeListPage_CompositionStyle,
-    EmployeeList
+    EmployeeList,
+    Home,
   },
 };
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello employees</h1>
+
+    <EmployeeList :employee="renderedEmployees"/>
+  </div>
+</template>
+
+<script>
+import EmployeeList from '../components/EmployeeList.vue';
+import {computed} from 'vue'
+
+export default {
+  components: {
+    EmployeeList,
+  },
+
+}
+</script>
